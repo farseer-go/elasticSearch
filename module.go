50101f46e24ef6ff6dc723dd9483d8cf90fd0a12
steden <1470804@qq.com>
2023-03-07 17:33:33 +0800

    使用v0.8.0的模块接口

--- module.go
+++ module.go
@@ -8,15 +8,3 @@ type Module struct {
 func (module Module) DependsModule() []modules.FarseerModule {
 	return nil
 }
-
-func (module Module) PreInitialize() {
-}
-
-func (module Module) Initialize() {
-}
-
-func (module Module) PostInitialize() {
-}
-
-func (module Module) Shutdown() {
-}
