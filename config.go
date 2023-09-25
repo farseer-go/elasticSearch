b195f095f165056f75aa1ec5570334e984527570
jk <jk@jk.com>
2023-09-25 22:59:37 +0800

    完善ES说明文档

--- config.go
+++ config.go
@@ -2,11 +2,11 @@ package elasticSearch
 
 // elasticSearch配置结构
 type elasticConfig struct {
-	Server          string
-	Username        string
-	Password        string
-	ReplicasCount   int
-	ShardsCount     int
-	RefreshInterval int
-	IndexFormat     string
+	Server          string // 服务器地址
+	Username        string // 用户名称
+	Password        string // 用户密码
+	ReplicasCount   int    // 副本数量
+	ShardsCount     int    // 碎片数量
+	RefreshInterval int    // 刷新间隔
+	IndexFormat     string // 索引格式
 }
